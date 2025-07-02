946209cf42631c0e11bc1047e589923000031e3b
hmzgt <hmzgtl16@gmail.com>
2025-07-02 18:26:36 +0100

    add all plugins in project build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.androidx.room) apply false
+    alias(libs.plugins.com.android.application) apply false
+    alias(libs.plugins.com.android.library) apply false
+    alias(libs.plugins.com.android.test) apply false
+    alias(libs.plugins.com.google.dagger.hilt) apply false
+    alias(libs.plugins.com.google.devtools.ksp) apply false
+    alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.org.jetbrains.kotlin.compose) apply false
+    alias(libs.plugins.org.jetbrains.kotlin.jvm) apply false
+    alias(libs.plugins.org.jetbrains.kotlin.serialization) apply false
 }
+
