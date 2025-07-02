cf8b8d6c86ee84c0a1f20bf7ff9519db3b63864c
hmzgt <hmzgtl16@gmail.com>
2025-07-02 18:57:41 +0100

    add AndroidApplicationFlavorsConventionPlugin

---
+++ build-logic/convention/src/main/kotlin/com/example/hnote/Flavors.kt
@@ -0,0 +1,40 @@
+package com.example.hnote
+
+import com.android.build.api.dsl.ApplicationExtension
+import com.android.build.api.dsl.ApplicationProductFlavor
+import com.android.build.api.dsl.CommonExtension
+import com.android.build.api.dsl.ProductFlavor
+
+enum class FlavorDimension {
+    CONTENT_TYPE
+}
+
+enum class NiaFlavor(val dimension: FlavorDimension, val applicationIdSuffix: String? = null) {
+    DEMO(FlavorDimension.CONTENT_TYPE, applicationIdSuffix = ".demo"),
+    PROD(FlavorDimension.CONTENT_TYPE),
+}
+
+fun configureFlavors(
+    commonExtension: CommonExtension<*, *, *, *, *, *>,
+    flavorConfigurationBlock: ProductFlavor.(flavor: NiaFlavor) -> Unit = {},
+) {
+    commonExtension.apply {
+        FlavorDimension.values().forEach { flavorDimension ->
+            flavorDimensions += flavorDimension.name
+        }
+
+        productFlavors {
+            NiaFlavor.values().forEach { niaFlavor ->
+                register(niaFlavor.name) {
+                    dimension = niaFlavor.dimension.name
+                    flavorConfigurationBlock(this, niaFlavor)
+                    if (this@apply is ApplicationExtension && this is ApplicationProductFlavor) {
+                        if (niaFlavor.applicationIdSuffix != null) {
+                            applicationIdSuffix = niaFlavor.applicationIdSuffix
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
