572236854a065aaa6f3bdd8329b1ba2cfc526985
hmzgt <hmzgtl16@gmail.com>
2025-07-03 14:05:48 +0100

    add NoteWithItems

---
+++ core/database/src/main/kotlin/com/example/hnote/core/database/model/NoteWithItems.kt
@@ -0,0 +1,13 @@
+package com.example.hnote.core.database.model
+
+import androidx.room.Embedded
+import androidx.room.Relation
+
+data class NoteWithItems(
+    @Embedded val note: NoteEntity,
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "noteId"
+    )
+    val items: List<ItemEntity>
+)
