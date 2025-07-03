7b59cf249dcea5498f484a2b7872eb1b5111afaa
hmzgt <hmzgtl16@gmail.com>
2025-07-03 23:13:46 +0100

    Add UserDataRepository binding to DataModule
    This commit introduces a binding for `UserDataRepository` to `UserDataRepositoryImpl` within the `DataModule`. This allows for dependency injection of the user data repository throughout the application.

--- core/data/src/main/kotlin/com/example/hnote/core/data/di/DataModule.kt
+++ core/data/src/main/kotlin/com/example/hnote/core/data/di/DataModule.kt
@@ -1,5 +1,8 @@
 package com.example.hnote.core.data.di
 
+import com.example.hnote.core.data.repository.UserDataRepository
+import com.example.hnote.core.data.repository.UserDataRepositoryImpl
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
@@ -8,5 +11,8 @@ import dagger.hilt.components.SingletonComponent
 @InstallIn(SingletonComponent::class)
 abstract class DataModule {
 
-
+    @Binds
+    internal abstract fun bindsUserDataRepository(
+        userDataRepository: UserDataRepositoryImpl
+    ): UserDataRepository
 }
