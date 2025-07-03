c2c20740fc9dc6f43b38cebb42e14deb42e41dbb
hmzgt <hmzgtl16@gmail.com>
2025-07-03 23:03:03 +0100

    Implement UserDataRepository

--- core/data/src/main/kotlin/com/example/hnote/core/data/repository/UserDataRepositoryImpl.kt
+++ core/data/src/main/kotlin/com/example/hnote/core/data/repository/UserDataRepositoryImpl.kt
@@ -1,4 +1,21 @@
 package com.example.hnote.core.data.repository
 
-class UserDataRepositoryImpl {
+import com.example.hnote.core.model.Theme
+import com.example.hnote.core.model.UserData
+import kotlinx.coroutines.flow.Flow
+
+class UserDataRepositoryImpl : UserDataRepository {
+
+    override val userData: Flow<UserData>
+        get() = TODO("Not yet implemented")
+
+    override suspend fun setTheme(theme: Theme) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun setDynamicColorPreference(useDynamicColor: Boolean) {
+        TODO("Not yet implemented")
+    }
+    
+
 }
