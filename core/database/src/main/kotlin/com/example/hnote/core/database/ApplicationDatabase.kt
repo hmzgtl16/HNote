5691e03ed4946e1c77ee61e683978737fe003a6b
hmzgt <hmzgtl16@gmail.com>
2025-07-03 14:33:38 +0100

    add ApplicationDatabase

---
+++ core/database/src/main/kotlin/com/example/hnote/core/database/ApplicationDatabase.kt
@@ -0,0 +1,32 @@
+package com.example.hnote.core.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.hnote.core.database.dao.NoteDao
+import com.example.hnote.core.database.model.ItemEntity
+import com.example.hnote.core.database.model.NoteEntity
+import com.example.hnote.core.database.model.SearchQueryEntity
+import com.example.hnote.core.database.util.InstantConverter
+import com.example.hnote.core.database.util.NoteTypeConverter
+import com.example.hnote.core.database.util.ReminderRepeatModeConverter
+
+@Database(
+    entities = [
+        NoteEntity::class,
+        ItemEntity::class,
+        SearchQueryEntity::class
+    ],
+    version = 1,
+    exportSchema = true
+)
+@TypeConverters(
+    value = [
+        InstantConverter::class,
+        NoteTypeConverter::class,
+        ReminderRepeatModeConverter::class
+    ]
+)
+internal abstract class ApplicationDatabase : RoomDatabase() {
+    abstract fun noteDao(): NoteDao
+}
