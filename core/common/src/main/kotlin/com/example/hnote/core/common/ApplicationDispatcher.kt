313039912aa552240cd396344324b6ce6d298c8d
hmzgt <hmzgtl16@gmail.com>
2025-07-03 10:21:16 +0100

    add ApplicationDispatcher

---
+++ core/common/src/main/kotlin/com/example/hnote/core/common/ApplicationDispatcher.kt
@@ -0,0 +1,12 @@
+package com.example.hnote.core.common
+
+import javax.inject.Qualifier
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class ApplicationDispatcher(val dispatcher: Dispatcher)
+
+enum class Dispatcher {
+    DEFAULT,
+    IO
+}
