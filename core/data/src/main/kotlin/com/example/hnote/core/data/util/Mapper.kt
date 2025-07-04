ab0b6526c42a8801e46bd4b8661cf3b81454f01e
hmzgt <hmzgtl16@gmail.com>
2025-07-04 11:46:23 +0100

    add Mapper between domain and database layer

---
+++ core/data/src/main/kotlin/com/example/hnote/core/data/util/Mapper.kt
@@ -0,0 +1,84 @@
+package com.example.hnote.core.data.util
+
+import com.example.hnote.core.database.model.ItemEntity
+import com.example.hnote.core.database.model.NoteEntity
+import com.example.hnote.core.database.model.NoteWithItems
+import com.example.hnote.core.model.Item
+import com.example.hnote.core.model.Note
+import com.example.hnote.core.model.NoteType
+import com.example.hnote.core.model.ReminderRepeatMode
+import com.example.hnote.core.database.util.NoteType as NoteTypeEntity
+import com.example.hnote.core.database.util.ReminderRepeatMode as ReminderRepeatModeEntity
+
+fun Note.toEntity(): NoteEntity = NoteEntity(
+    id = this.id,
+    title = this.title,
+    content = this.content,
+    pinned = this.pinned,
+    backgroundColor = backgroundColor,
+    createdAt = created,
+    updatedAt = updated,
+    type = type.toEntity(),
+    reminder = reminder,
+    reminderMode = reminderRepeatMode?.toEntity(),
+    completed = completed
+)
+
+fun Note.toItemEntities(): List<ItemEntity> = items
+    .map { it.toEntity(noteId = id) }
+
+fun Item.toEntity(noteId: Long = 0L): ItemEntity = ItemEntity(
+    id = this.id,
+    content = this.content,
+    checked = this.checked,
+    noteId = noteId
+)
+
+fun ItemEntity.toModel(): Item = Item(
+    id = this.id,
+    content = this.content,
+    checked = this.checked
+)
+
+fun NoteWithItems.toModel(): Note = Note(
+    id = note.id,
+    title = note.title,
+    content = note.content,
+    pinned = note.pinned,
+    backgroundColor = note.backgroundColor,
+    created = note.createdAt,
+    updated = note.updatedAt,
+    type = note.type.toModel(),
+    reminder = note.reminder,
+    reminderRepeatMode = note.reminderMode?.toModel(),
+    completed = note.completed,
+    items = items.map(ItemEntity::toModel)
+)
+
+fun NoteType.toEntity(): NoteTypeEntity = when (this) {
+    NoteType.SIMPLE -> NoteTypeEntity.SIMPLE
+    NoteType.REMINDER -> NoteTypeEntity.REMINDER
+    NoteType.CHECK_LIST -> NoteTypeEntity.CHECK_LIST
+}
+
+fun NoteTypeEntity.toModel(): NoteType = when (this) {
+    NoteTypeEntity.SIMPLE -> NoteType.SIMPLE
+    NoteTypeEntity.REMINDER -> NoteType.REMINDER
+    NoteTypeEntity.CHECK_LIST -> NoteType.CHECK_LIST
+}
+
+fun ReminderRepeatMode.toEntity(): ReminderRepeatModeEntity = when (this) {
+    ReminderRepeatMode.NONE -> ReminderRepeatModeEntity.NONE
+    ReminderRepeatMode.DAILY -> ReminderRepeatModeEntity.DAILY
+    ReminderRepeatMode.WEEKLY -> ReminderRepeatModeEntity.WEEKLY
+    ReminderRepeatMode.MONTHLY -> ReminderRepeatModeEntity.MONTHLY
+    ReminderRepeatMode.YEARLY -> ReminderRepeatModeEntity.YEARLY
+}
+
+fun ReminderRepeatModeEntity.toModel(): ReminderRepeatMode = when (this) {
+    ReminderRepeatModeEntity.NONE -> ReminderRepeatMode.NONE
+    ReminderRepeatModeEntity.DAILY -> ReminderRepeatMode.DAILY
+    ReminderRepeatModeEntity.WEEKLY -> ReminderRepeatMode.WEEKLY
+    ReminderRepeatModeEntity.MONTHLY -> ReminderRepeatMode.MONTHLY
+    ReminderRepeatModeEntity.YEARLY -> ReminderRepeatMode.YEARLY
+}
