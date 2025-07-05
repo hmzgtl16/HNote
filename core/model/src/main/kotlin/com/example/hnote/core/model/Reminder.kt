048e23945e2ef0cf7792d27f37286a6c410dc192
hmzgt <hmzgtl16@gmail.com>
2025-07-06 00:00:41 +0100

    Rename reminderRepeatMode to repeatMode in Reminder

--- core/model/src/main/kotlin/com/example/hnote/core/model/Reminder.kt
+++ core/model/src/main/kotlin/com/example/hnote/core/model/Reminder.kt
@@ -4,6 +4,6 @@ import kotlinx.datetime.Instant
 
 data class Reminder(
     val reminder: Instant,
-    val reminderRepeatMode: ReminderRepeatMode = ReminderRepeatMode.NONE,
-    val completed: Boolean = false,
+    val repeatMode: ReminderRepeatMode = ReminderRepeatMode.NONE,
+    val completed: Boolean = false
 )
