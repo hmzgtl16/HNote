c93b2a18048e0b5262987f1a6e6c1114ee5fb419
hmzgt <hmzgtl16@gmail.com>
2025-07-05 11:39:49 +0100

    add NoteCard
    This commit introduces the `NoteCard` composable, a UI element designed to display note information. It also includes `NotesPreviewParameterProvider` for previewing various note states and a string resource for untitled notes.
    
    The `NoteCard` composable can be used in two ways:
    - With multi-selection: Allows users to select multiple notes and perform actions like pinning.
    - Without multi-selection: Displays individual note details and allows navigation to the note.
    
    Key features of `NoteCard`:
    - Displays note title and content.
    - Supports custom background colors.
    - Handles untitled notes gracefully.
    - Provides visual cues for pinned notes.
    - Integrates with multi-selection functionality.

---
+++ core/ui/src/main/kotlin/com/example/hnote/core/ui/NoteCard.kt
@@ -0,0 +1,218 @@
+package com.example.hnote.core.ui
+
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.onClick
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
+import com.example.hnote.core.design.component.AppIconToggleButton
+import com.example.hnote.core.design.component.ThemePreviews
+import com.example.hnote.core.design.icon.AppIcons
+import com.example.hnote.core.design.theme.AppTheme
+import com.example.hnote.core.design.theme.LocalTintTheme
+import com.example.hnote.core.model.Note
+
+@Composable
+fun NoteCard(
+    note: Note,
+    multiSelectionEnabled: Boolean,
+    enableMultiSelection: () -> Unit,
+    selected: Boolean,
+    onSelectedChanged: (Note) -> Unit,
+    onNoteClick: (Long) -> Unit,
+    onPinClick: (Note) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+
+    ElevatedCard(
+        colors = if (note.backgroundColor != null)
+            CardDefaults.elevatedCardColors(containerColor = Color(color = note.backgroundColor!!))
+        else CardDefaults.elevatedCardColors(),
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentHeight(align = Alignment.Top)
+            .combinedClickable(
+                onClick = {
+                    if (multiSelectionEnabled) onSelectedChanged(note)
+                    else onNoteClick(note.id)
+                },
+                onLongClick = {
+                    if (multiSelectionEnabled) return@combinedClickable
+                    enableMultiSelection()
+                    onSelectedChanged(note)
+                },
+                role = Role.RadioButton
+            )
+            .semantics { onClick(label = "Open Note", action = null) },
+        content = {
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(all = 16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(
+                    space = 8.dp,
+                    alignment = Alignment.CenterVertically
+                ),
+                content = {
+
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.spacedBy(
+                            space = 8.dp,
+                            alignment = Alignment.Start
+                        ),
+                        content = {
+                            Text(
+                                text = note.title.ifEmpty(
+                                    defaultValue = { stringResource(id = R.string.core_ui_note_untitled) }
+                                ),
+                                style = MaterialTheme.typography.titleMedium,
+                                textAlign = TextAlign.Start,
+                                modifier = Modifier.weight(weight = 1f)
+                            )
+
+                            if (!multiSelectionEnabled) {
+
+                                AppIconToggleButton(
+                                    checked = note.pinned,
+                                    onCheckedChange = { onPinClick(note) },
+                                    icon = {
+                                        Icon(
+                                            imageVector = AppIcons.PinBorder,
+                                            contentDescription = "Pinned"
+                                        )
+                                    },
+                                    checkedIcon = {
+                                        Icon(
+                                            imageVector = AppIcons.Pin,
+                                            contentDescription = "Unpinned"
+                                        )
+                                    }
+                                )
+                            }
+
+                            if (multiSelectionEnabled) {
+
+                                RadioButton(
+                                    selected = selected,
+                                    onClick = null,
+                                    colors = RadioButtonDefaults.colors(
+                                        selectedColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                                        unselectedColor = LocalTintTheme.current.iconTint
+                                    )
+                                )
+                            }
+                        }
+                    )
+
+                    if (note.content.isNotEmpty()) {
+                        Text(
+                            text = note.content,
+                            style = MaterialTheme.typography.bodyMedium,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    }
+                }
+            )
+        }
+    )
+}
+
+@Composable
+fun NoteCard(
+    note: Note,
+    onNoteClick: (Long) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+
+    ElevatedCard(
+        onClick = { onNoteClick(note.id) },
+        colors = if (note.backgroundColor != null)
+            CardDefaults.elevatedCardColors(containerColor = Color(color = note.backgroundColor!!))
+        else CardDefaults.elevatedCardColors(),
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentHeight(align = Alignment.Top)
+            .semantics { onClick(label = "Open Note", action = null) },
+        content = {
+
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                content = {
+
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(all = 16.dp),
+                        verticalArrangement = Arrangement.spacedBy(
+                            space = 8.dp,
+                            alignment = Alignment.CenterVertically
+                        ),
+                        content = {
+
+                            Text(
+                                text = note.title.ifEmpty(
+                                    defaultValue = { stringResource(id = R.string.core_ui_note_untitled) }
+                                ),
+                                style = MaterialTheme.typography.titleMedium,
+                                textAlign = TextAlign.Start,
+                                modifier = Modifier.fillMaxWidth()
+                            )
+
+                            if (note.content.isNotEmpty()) {
+                                Text(
+                                    text = note.content,
+                                    style = MaterialTheme.typography.bodyMedium,
+                                    modifier = Modifier.fillMaxWidth()
+                                )
+                            }
+                        }
+                    )
+                }
+            )
+        }
+    )
+}
+
+@ThemePreviews
+@Composable
+fun NoteCardPreview(
+    @PreviewParameter(NotesPreviewParameterProvider::class)
+    notes: Map<Boolean, List<Note>>,
+) {
+    AppTheme {
+        NoteCard(
+            note = notes[true]!![1],
+            multiSelectionEnabled = true,
+            enableMultiSelection = {},
+            selected = true,
+            onSelectedChanged = {},
+            onNoteClick = {},
+            onPinClick = {}
+        )
+    }
+}
---
+++ core/ui/src/main/kotlin/com/example/hnote/core/ui/NotesPreviewParameterProvider.kt
@@ -0,0 +1,193 @@
+package com.example.hnote.core.ui
+
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import com.example.hnote.core.model.Item
+import com.example.hnote.core.model.Note
+import com.example.hnote.core.model.NoteType
+import com.example.hnote.core.model.ReminderRepeatMode
+import kotlinx.datetime.Instant
+
+class NotesPreviewParameterProvider : PreviewParameterProvider<Map<Boolean, List<Note>>> {
+    override val values: Sequence<Map<Boolean, List<Note>>>
+        get() = sequenceOf(
+            mapOf(
+                true to listOf(
+                    Note(
+                        id = 1L,
+                        title = "Note 1",
+                        content = "Content of note 1",
+                        backgroundColor = -8972498,
+                        type = NoteType.SIMPLE,
+                        pinned = true
+                    ),
+                    Note(
+                        id = 2L,
+                        title = "Note 2",
+                        content = "Content of note 2",
+                        type = NoteType.SIMPLE
+                    ),
+                    Note(
+                        id = 3L,
+                        title = "Note 3",
+                        content = "Content of note 3",
+                        type = NoteType.SIMPLE
+                    ),
+                    Note(
+                        id = 4L,
+                        title = "Note 4",
+                        content = "Content of note 4",
+                        backgroundColor = -8972498,
+                        type = NoteType.SIMPLE
+                    ),
+                    Note(
+                        id = 5L,
+                        title = "Note 5",
+                        content = "Content of note 5",
+                        type = NoteType.REMINDER,
+                        pinned = true,
+                        reminder = Instant.parse("2024-10-01T12:00:00Z"),
+                        reminderRepeatMode = ReminderRepeatMode.NONE,
+                        completed = false,
+                    ),
+                    Note(
+                        id = 6L,
+                        title = "Note 6",
+                        content = "Content of note 6",
+                        type = NoteType.REMINDER,
+                        pinned = true,
+                        reminder = Instant.parse("2024-10-01T12:00:00Z"),
+                        reminderRepeatMode = ReminderRepeatMode.DAILY,
+                        completed = true,
+                    ),
+                    Note(
+                        id = 7L,
+                        title = "Note 7",
+                        content = "Content of note 7",
+                        type = NoteType.REMINDER,
+                        pinned = true,
+                        items = listOf(
+                            Item(
+                                id = 1L,
+                                content = "Item 1",
+                                checked = false
+                            ),
+                            Item(
+                                id = 2L,
+                                content = "Item 2",
+                                checked = true
+                            ),
+                            Item(
+                                id = 3L,
+                                content = "Item 3",
+                                checked = false
+                            )
+                        )
+                    ),
+                    Note(
+                        id = 8L,
+                        title = "Note 8",
+                        content = "Content of note 8",
+                        type = NoteType.REMINDER,
+                        pinned = true,
+                        items = listOf(
+                            Item(
+                                id = 4L,
+                                content = "Item 4",
+                                checked = false
+                            ),
+                            Item(
+                                id = 5L,
+                                content = "Item 5",
+                                checked = true
+                            ),
+                            Item(
+                                id = 6L,
+                                content = "Item 6",
+                                checked = false
+                            )
+                        )
+                    )
+                ),
+                false to listOf(
+                    Note(
+                        id = 9L,
+                        title = "Note 9",
+                        content = "Content of note 9",
+                        type = NoteType.SIMPLE,
+                        pinned = false
+                    ),
+                    Note(
+                        id = 10L,
+                        title = "Note 10",
+                        content = "Content of note 10",
+                        type = NoteType.SIMPLE,
+                        pinned = false
+                    ),
+                    Note(
+                        id = 11L,
+                        title = "Note 11",
+                        content = "Content of note 11",
+                        type = NoteType.SIMPLE,
+                        pinned = false,
+                        reminder = Instant.parse("2024-10-01T12:00:00Z"),
+                        reminderRepeatMode = ReminderRepeatMode.DAILY
+                    ),
+                    Note(
+                        id = 12L,
+                        title = "Note 12",
+                        content = "Content of note 12",
+                        type = NoteType.SIMPLE,
+                        pinned = false,
+                        reminder = Instant.parse("2024-10-01T12:00:00Z"),
+                        reminderRepeatMode = ReminderRepeatMode.WEEKLY
+                    ),
+                    Note(
+                        id = 13L,
+                        title = "Note 13",
+                        content = "Content of note 13",
+                        type = NoteType.SIMPLE,
+                        items = listOf(
+                            Item(
+                                id = 7L,
+                                content = "Item 7",
+                                checked = false
+                            ),
+                            Item(
+                                id = 8L,
+                                content = "Item 8",
+                                checked = true
+                            ),
+                            Item(
+                                id = 9L,
+                                content = "Item 9",
+                                checked = false
+                            )
+                        )
+                    ),
+                    Note(
+                        id = 14L,
+                        title = "Note 14",
+                        content = "Content of note 14",
+                        type = NoteType.SIMPLE,
+                        items = listOf(
+                            Item(
+                                id = 10L,
+                                content = "Item 10",
+                                checked = false
+                            ),
+                            Item(
+                                id = 11L,
+                                content = "Item 11",
+                                checked = true
+                            ),
+                            Item(
+                                id = 12L,
+                                content = "Item 12",
+                                checked = false
+                            )
+                        )
+                    )
+                )
+            )
+        )
+}
