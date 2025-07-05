c25845b3c507073fe2aa8264012e14df65e98601
hmzgt <hmzgtl16@gmail.com>
2025-07-05 16:17:44 +0100

    add TopAppBar component

---
+++ core/design/src/main/kotlin/com/example/hnote/core/design/component/TopAppBar.kt
@@ -0,0 +1,224 @@
+package com.example.hnote.core.design.component
+
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SearchBarDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import com.example.hnote.core.design.icon.AppIcons
+import com.example.hnote.core.design.theme.AppTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppTopAppBar(
+    title: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+    navigationIcon: @Composable () -> Unit,
+    actions: @Composable RowScope.() -> Unit,
+    colors: TopAppBarColors = TopAppBarDefaults.centerAlignedTopAppBarColors(),
+    isCenterAligned: Boolean = true,
+) {
+
+    if (isCenterAligned) {
+        CenterAlignedTopAppBar(
+            title = title,
+            modifier = modifier,
+            navigationIcon = navigationIcon,
+            actions = actions,
+            colors = colors
+        )
+    }
+
+    if (!isCenterAligned) {
+        TopAppBar(
+            title = title,
+            modifier = modifier,
+            navigationIcon = navigationIcon,
+            actions = actions,
+            colors = colors
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppSearchAppBar(
+    searchBar: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+    navigationIcon: @Composable () -> Unit,
+    colors: TopAppBarColors = TopAppBarDefaults.topAppBarColors(),
+) {
+
+    TopAppBar(
+        title = searchBar,
+        modifier = modifier,
+        navigationIcon = navigationIcon,
+        colors = colors
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@ThemePreviews
+@Composable
+private fun AppSingleActionTopAppBarPreview() {
+    AppTheme {
+        AppTopAppBar(
+            title = {
+                Text(
+                    text = stringResource(id = android.R.string.untitled),
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            },
+            navigationIcon = {
+                IconButton(
+                    onClick = {},
+                    content = {
+                        Icon(
+                            imageVector = AppIcons.Search,
+                            contentDescription = "Navigation icon",
+                            tint = MaterialTheme.colorScheme.onSurface,
+                        )
+                    }
+                )
+            },
+            actions = {
+                IconButton(
+                    onClick = {},
+                    content = {
+                        Icon(
+                            imageVector = AppIcons.Settings,
+                            contentDescription = "Action icon",
+                            tint = MaterialTheme.colorScheme.onSurface,
+                        )
+                    },
+                )
+            }
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@ThemePreviews
+@Composable
+private fun AppMultiActionsTopAppBarPreview() {
+    AppTheme {
+        AppTopAppBar(
+            title = {
+                Text(
+                    text = stringResource(id = android.R.string.untitled),
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            },
+            navigationIcon = {
+                IconButton(
+                    onClick = {},
+                    content = {
+                        Icon(
+                            imageVector = AppIcons.Close,
+                            contentDescription = "Navigation icon",
+                            tint = MaterialTheme.colorScheme.onSurface,
+                        )
+                    }
+                )
+            },
+            actions = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = AppIcons.PinBorder,
+                        contentDescription = "Action 3",
+                        tint = MaterialTheme.colorScheme.onSurface,
+                    )
+                }
+
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = AppIcons.Delete,
+                        contentDescription = "Action 3",
+                        tint = MaterialTheme.colorScheme.onSurface,
+                    )
+                }
+
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = AppIcons.MoreVert,
+                        contentDescription = "Action 3",
+                        tint = MaterialTheme.colorScheme.onSurface,
+                    )
+                }
+            },
+            isCenterAligned = false
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@ThemePreviews
+@Composable
+private fun AppSearchAppBarPreview() {
+    AppTheme {
+        AppSearchAppBar(
+            searchBar = {
+                AppSearchBar(
+                    inputField = {
+                        SearchBarDefaults.InputField(
+                            query = "",
+                            onQueryChange = {},
+                            onSearch = {},
+                            expanded = false,
+                            onExpandedChange = {},
+                            placeholder = {
+                                Text(text = stringResource(id = android.R.string.search_go))
+                            },
+                            leadingIcon = {
+                                Icon(
+                                    imageVector = AppIcons.Search,
+                                    contentDescription = null,
+                                    tint = MaterialTheme.colorScheme.onSurface
+                                )
+                            },
+                            trailingIcon = {
+                                AppIconButton(
+                                    onClick = {},
+                                    icon = {
+                                        Icon(
+                                            imageVector = AppIcons.Close,
+                                            contentDescription = null,
+                                            tint = MaterialTheme.colorScheme.onSurface
+                                        )
+                                    }
+                                )
+                            }
+                        )
+                    },
+                    expanded = false,
+                    onExpandedChange = {},
+                    content = {}
+                )
+            },
+            navigationIcon = {
+                IconButton(
+                    onClick = {},
+                    content = {
+                        Icon(
+                            imageVector = AppIcons.Back,
+                            contentDescription = "Navigation icon",
+                            tint = MaterialTheme.colorScheme.onSurface,
+                        )
+                    }
+                )
+            }
+        )
+    }
+}
