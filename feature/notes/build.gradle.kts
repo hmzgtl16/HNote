4205830225cb409045d428d4737c1baeae47997a
hmzgt <hmzgtl16@gmail.com>
2025-07-05 12:20:09 +0100

    add `notes` feature module

---
+++ feature/notes/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    alias(libs.plugins.hnote.android.feature)
+    alias(libs.plugins.hnote.android.library.compose)
+}
+
+android {
+    namespace = "com.example.notes.feature.notes"
+}
+
+dependencies {
+    implementation(projects.core.data)
+}
