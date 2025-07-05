31c5984b27bd1a3c2e5bd248780a8d1713a75ed0
hmzgt <hmzgtl16@gmail.com>
2025-07-05 13:15:43 +0100

    Add common feature dependencies to AndroidFeatureConventionPlugin
    This commit adds common dependencies required by feature modules to the `AndroidFeatureConventionPlugin`. These include:
    
    - Core UI and Design modules
    - Hilt Navigation Compose
    - Lifecycle Runtime Compose
    - Lifecycle ViewModel Compose
    - Navigation Compose
    - Tracing KTX
    - Kotlinx Serialization JSON
    
    The corresponding versions for these dependencies have also been added to the `libs.versions.toml` file.

--- build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
@@ -1,8 +1,10 @@
 import com.android.build.gradle.LibraryExtension
+import com.example.hnote.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
 
 class AndroidFeatureConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -13,6 +15,24 @@ class AndroidFeatureConventionPlugin : Plugin<Project> {
 
             extensions.configure<LibraryExtension> {
                 testOptions.animationsDisabled = true
+            }
+
+            dependencies {
+                add("implementation", project(":core:ui"))
+                add("implementation", project(":core:design"))
+
+                add("implementation", libs.findLibrary("androidx.hilt.navigation.compose").get())
+                add("implementation", libs.findLibrary("androidx.lifecycle.runtime.compose").get())
+                add(
+                    "implementation",
+                    libs.findLibrary("androidx.lifecycle.viewmodel.compose").get()
+                )
+                add("implementation", libs.findLibrary("androidx.navigation.compose").get())
+                add("implementation", libs.findLibrary("androidx.tracing.ktx").get())
+                add(
+                    "implementation",
+                    libs.findLibrary("org.jetbrains.kotlinx.serialization.json").get()
+                )
             }
         }
     }
