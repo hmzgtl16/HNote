b7665e0947b55e91f68df015b1538512227363b0
hmzgt <hmzgtl16@gmail.com>
2025-07-06 19:48:59 +0100

    add ItemCallback and NoteCallback for RoomDatabase
    This commit introduces `ItemCallback` and `NoteCallback` classes, which extend `RoomDatabase.Callback`.
    
    These callbacks define SQL triggers that are executed when the database is created (`onCreate`). The triggers automatically manage corresponding Full-Text Search (FTS) tables (`items_fts` and `notes_fts`) when data is inserted, updated, or deleted in the `items` and `notes` tables, respectively.

---
+++ core/database/src/main/kotlin/com/example/hnote/core/database/util/NoteCallback.kt
@@ -0,0 +1,44 @@
+package com.example.hnote.core.database.util
+
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+
+val NoteCallback = object : RoomDatabase.Callback() {
+
+    override fun onCreate(db: SupportSQLiteDatabase) {
+        super.onCreate(db)
+        db.apply {
+            execSQL(sql = noteInsertTrigger)
+            db.execSQL(sql = noteUpdateTrigger)
+            db.execSQL(sql = noteDeleteTrigger)
+        }
+    }
+}
+
+private const val noteInsertTrigger =
+    """
+        CREATE TRIGGER IF NOT EXISTS insert_note_fts AFTER INSERT ON notes
+        BEGIN 
+            INSERT INTO notes_fts(note_fts_id, note_fts_title, note_fts_content)
+            VALUES (new.id, new.title, new.content);
+        END;
+    """
+
+private const val noteUpdateTrigger =
+    """
+        CREATE TRIGGER IF NOT EXISTS update_note_fts AFTER UPDATE ON notes
+        BEGIN 
+            UPDATE notes_fts
+            SET note_fts_title = new.title, note_fts_content = new.content
+            WHERE note_fts_id = new.id;
+        END;
+    """
+
+private const val noteDeleteTrigger =
+    """
+        CREATE TRIGGER IF NOT EXISTS delete_note_fts AFTER DELETE ON notes
+        BEGIN 
+            DELETE FROM notes_fts
+            WHERE note_fts_id = old.id;
+        END;
+    """
