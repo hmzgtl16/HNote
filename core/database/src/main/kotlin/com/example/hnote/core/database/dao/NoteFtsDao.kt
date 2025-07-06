36922a6920401c714f879a3970d5c053d587438f
hmzgt <hmzgtl16@gmail.com>
2025-07-06 23:32:58 +0100

    add NoteFtsDao

---
+++ core/database/src/main/kotlin/com/example/hnote/core/database/dao/NoteFtsDao.kt
@@ -0,0 +1,12 @@
+package com.example.hnote.core.database.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoteFtsDao {
+
+    @Query("SELECT note_fts_id FROM notes_fts WHERE notes_fts MATCH :text")
+    fun searchAllNotes(text: String): Flow<List<String>>
+}
