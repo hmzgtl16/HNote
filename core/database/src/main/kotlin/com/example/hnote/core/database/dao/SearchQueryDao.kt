eb5c67443f6fdbcbda5e78c782b2bd4bbf7f9368
hmzgt <hmzgtl16@gmail.com>
2025-07-06 19:58:24 +0100

    add SearchQueryDao

---
+++ core/database/src/main/kotlin/com/example/hnote/core/database/dao/SearchQueryDao.kt
@@ -0,0 +1,24 @@
+package com.example.hnote.core.database.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import com.example.hnote.core.database.model.SearchQueryEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SearchQueryDao {
+
+    @Query(value = "SELECT * FROM search_queries ORDER BY queried DESC LIMIT :limit")
+    fun getSearchQueries(limit: Int): Flow<List<SearchQueryEntity>>
+
+    @Upsert
+    suspend fun upsertSearchQuery(searchQuery: SearchQueryEntity)
+
+    @Query(value = "DELETE FROM search_queries")
+    suspend fun deleteAll()
+
+    @Delete
+    suspend fun delete(searchQueryEntity: SearchQueryEntity)
+}
