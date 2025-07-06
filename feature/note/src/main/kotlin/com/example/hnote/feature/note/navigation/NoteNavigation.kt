397c33a64218fff2fa76451a6e6412b1adba399b
hmzgt <hmzgtl16@gmail.com>
2025-07-06 19:01:25 +0100

    Add NoteNavigation
    This commit introduces the `NoteNavigation` for the note feature.
    
    It includes:
    - `NoteRoute` data class for type-safe navigation with a `noteId`.
    - Extension functions for `NavController` to navigate to a specific note.
    - Extension functions for `NavGraphBuilder` to define the note screen within the navigation graph.

---
+++ feature/note/src/main/kotlin/com/example/hnote/feature/note/navigation/NoteNavigation.kt
@@ -0,0 +1,23 @@
+package com.example.hnote.feature.note.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.example.hnote.feature.note.NoteRoute
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class NoteRoute(val noteId: Long)
+
+fun NavController.navigateToNote(noteId: Long, navOptions: NavOptions) =
+    navigate(route = NoteRoute(noteId = noteId), navOptions = navOptions)
+
+fun NavGraphBuilder.noteScreen(
+    onBackClick: () -> Unit,
+    onShowSnackbar: suspend (String, String?) -> Boolean
+) {
+    composable<NoteRoute> {
+        NoteRoute(onBackClick = onBackClick)
+    }
+}
