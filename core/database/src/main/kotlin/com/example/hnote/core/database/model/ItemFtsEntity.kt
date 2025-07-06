6d48b60e3bde6731b546854da4b20c57e41c1d82
hmzgt <hmzgtl16@gmail.com>
2025-07-06 19:42:33 +0100

    introduces FTS (Full-Text Search) entities for `Item` and `Note` to enable searching within their content.

---
+++ core/database/src/main/kotlin/com/example/hnote/core/database/model/ItemFtsEntity.kt
@@ -0,0 +1,14 @@
+package com.example.hnote.core.database.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Fts4
+
+@Entity(tableName = "items_fts")
+@Fts4
+data class ItemFtsEntity(
+    @ColumnInfo(name = "item_fts_id")
+    val id: Long,
+    @ColumnInfo(name = "item_fts_content")
+    val content: String
+)
