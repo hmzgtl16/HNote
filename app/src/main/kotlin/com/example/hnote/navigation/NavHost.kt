3442e4d48e6f5b07575b0a5dd276a3ba6caa1198
hmzgt <hmzgtl16@gmail.com>
2025-07-06 17:25:07 +0100

    Remove compose and testing dependencies
    The compose and testing dependencies have been moved to the feature module.
    Added dependencies for navigation compose and kotlinx serialization json.

---
+++ app/src/main/kotlin/com/example/hnote/navigation/NavHost.kt
@@ -0,0 +1,11 @@
+package com.example.hnote.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AppNavHost(
+    modifier: Modifier = Modifier
+) {
+
+}
