e920cbc503bb91b7fa84f2ea205d6b6417381bc0
hmzgt <hmzgtl16@gmail.com>
2025-07-06 19:59:59 +0100

    add SearchQueryDao to DaoModule

--- core/database/src/main/kotlin/com/example/hnote/core/database/di/DaoModule.kt
+++ core/database/src/main/kotlin/com/example/hnote/core/database/di/DaoModule.kt
@@ -2,6 +2,7 @@ package com.example.hnote.core.database.di
 
 import com.example.hnote.core.database.ApplicationDatabase
 import com.example.hnote.core.database.dao.NoteDao
+import com.example.hnote.core.database.dao.SearchQueryDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -12,7 +13,10 @@ import dagger.hilt.components.SingletonComponent
 internal object DaoModule {
 
     @Provides
-    fun provideNoteDao(database: ApplicationDatabase): NoteDao {
-        return database.noteDao()
-    }
+    fun provideNoteDao(database: ApplicationDatabase): NoteDao =
+        database.noteDao()
+
+    @Provides
+    fun provideSearchQueryDao(database: ApplicationDatabase): SearchQueryDao =
+        database.searchQueryDao()
 }
