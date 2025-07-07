2a9c9ffd933d5c0cf1d79b9303f4dcdbe55e0588
hmzgt <hmzgtl16@gmail.com>
2025-07-07 10:31:11 +0100

    Add settings feature module
    This commit introduces a new Android feature module named `settings`.
    The module is configured with the `hnote.android.feature` and `hnote.android.library.compose` plugins.
    Basic test files and Proguard rules have also been added.
    
    Additionally, unused `appcompat` and `material` dependencies were removed from `libs.versions.toml`.

---
+++ feature/settings/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    alias(libs.plugins.hnote.android.feature)
+    alias(libs.plugins.hnote.android.library.compose)
+}
+
+android {
+    namespace = "com.example.hnote.feature.settings"
+}
+
+dependencies {
+
+}
