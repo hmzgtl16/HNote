3e5dadad6d92b4e9ddf007587ab3389578609270
hmzgt <hmzgtl16@gmail.com>
2025-07-07 18:00:02 +0100

    make AppState constructor parameters default

--- app/src/main/kotlin/com/example/hnote/ui/AppState.kt
+++ app/src/main/kotlin/com/example/hnote/ui/AppState.kt
@@ -3,10 +3,12 @@ package com.example.hnote.ui
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hasRoute
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.example.hnote.feature.search.navigation.navigateToSearch
 import com.example.hnote.feature.settings.navigation.navigateToSettings
 import com.example.notes.feature.notes.navigation.NotesRoute
@@ -31,8 +33,8 @@ class AppState(
 
 @Composable
 fun rememberAppState(
-    navController: NavHostController,
-    coroutineScope: CoroutineScope
+    navController: NavHostController = rememberNavController(),
+    coroutineScope: CoroutineScope = rememberCoroutineScope()
 ): AppState = remember(
     key1 = navController,
     key2 = coroutineScope,
