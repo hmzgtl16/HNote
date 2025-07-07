10364dbbce23c0c6f9d7a3e23ae3d34ef23e3578
hmzgt <hmzgtl16@gmail.com>
2025-07-07 12:14:12 +0100

    Search note & item ids separately

--- core/database/src/main/kotlin/com/example/hnote/core/database/dao/NoteFtsDao.kt
+++ core/database/src/main/kotlin/com/example/hnote/core/database/dao/NoteFtsDao.kt
@@ -6,6 +6,9 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NoteFtsDao {
-    @Query("SELECT note_fts_id FROM notes_fts, items_fts WHERE notes_fts MATCH :text or (items_fts MATCH :text and items_fts.item_fts_note_id = notes_fts.note_fts_id)")
+    @Query("SELECT note_fts_id FROM notes_fts, items_fts WHERE notes_fts MATCH :text")
     fun searchAllNotes(text: String): Flow<List<Long>>
+
+    @Query("SELECT item_fts_note_id FROM items_fts WHERE items_fts MATCH :text")
+    fun searchAllItems(text: String): Flow<List<Long>>
 }
