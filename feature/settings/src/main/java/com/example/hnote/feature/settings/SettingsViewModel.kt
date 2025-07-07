4618b7ef0c9031a3adebc79643678ab71f58e7e3
hmzgt <hmzgtl16@gmail.com>
2025-07-07 11:17:41 +0100

    feat(settings): add SettingsViewModel for managing user preferences

---
+++ feature/settings/src/main/java/com/example/hnote/feature/settings/SettingsViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.hnote.feature.settings
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.hnote.core.data.repository.UserDataRepository
+import com.example.hnote.core.model.Theme
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SettingsViewModel @Inject constructor(
+    private val userDataRepository: UserDataRepository,
+) : ViewModel() {
+
+
+    val uiState: StateFlow<SettingsUiState> = userDataRepository.userData
+        .map {
+            SettingsUiState.Success(
+                uiTheme = it.theme,
+                useDynamicColor = it.useDynamicColor
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 5000L),
+            initialValue = SettingsUiState.Loading,
+        )
+
+    fun updateTheme(theme: Theme) = viewModelScope.launch {
+        userDataRepository.setTheme(theme = theme)
+    }
+
+    fun updateDynamicColor(useDynamicColor: Boolean) = viewModelScope.launch {
+        userDataRepository.setDynamicColorPreference(useDynamicColor)
+    }
+}
+
