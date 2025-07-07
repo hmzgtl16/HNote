90a907ef04ad2f9b1bda9215c8b15a5277630727
hmzgt <hmzgtl16@gmail.com>
2025-07-07 16:41:17 +0100

    Update AppState with current destination and isMainDestination
    This commit updates the `AppState` class to include:
    - `currentDestination`: Provides the current navigation destination.
    - `isMainDestination`: A boolean flag that indicates if the current destination is the main notes route.

--- app/src/main/kotlin/com/example/hnote/ui/AppState.kt
+++ app/src/main/kotlin/com/example/hnote/ui/AppState.kt
@@ -3,9 +3,13 @@ package com.example.hnote.ui
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.remember
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hasRoute
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.hnote.feature.search.navigation.navigateToSearch
 import com.example.hnote.feature.settings.navigation.navigateToSettings
+import com.example.notes.feature.notes.navigation.NotesRoute
 import kotlinx.coroutines.CoroutineScope
 
 @Stable
@@ -13,6 +17,13 @@ class AppState(
     val navController: NavHostController,
     val coroutineScope: CoroutineScope
 ) {
+
+    val currentDestination: NavDestination?
+        @Composable get() = navController
+            .currentBackStackEntryAsState().value?.destination
+
+    val isMainDestination: Boolean
+        @Composable get() = currentDestination?.hasRoute(route = NotesRoute::class) ?: false
 
     fun navigateToSearch() = navController.navigateToSearch()
     fun navigateToSettings() = navController.navigateToSettings()
