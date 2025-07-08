e7f0d9b298f15c374caf12830e6113b7037ca426
hmzgt <hmzgtl16@gmail.com>
2025-07-08 17:11:44 +0100

    Raise minSdk from 24 to 26

--- build-logic/convention/src/main/kotlin/com/example/hnote/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/com/example/hnote/KotlinAndroid.kt
@@ -18,7 +18,7 @@ internal fun Project.configureKotlinAndroid(
         compileSdk = 36
 
         defaultConfig {
-            minSdk = 24
+            minSdk = 26
         }
 
         compileOptions {
