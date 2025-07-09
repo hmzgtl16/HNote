da61069da5104c1edffeda0cd487b50cd4dde05c
hmzgt <hmzgtl16@gmail.com>
2025-07-09 19:56:49 +0100

    remove NoteRoute onBackClick parameter

--- feature/note/src/main/kotlin/com/example/hnote/feature/note/navigation/NoteNavigation.kt
+++ feature/note/src/main/kotlin/com/example/hnote/feature/note/navigation/NoteNavigation.kt
@@ -1,23 +1,14 @@
 package com.example.hnote.feature.note.navigation
 
-import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import com.example.hnote.core.navigation.Route
 import com.example.hnote.feature.note.NoteRoute
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class NoteRoute(val noteId: Long)
-
-fun NavController.navigateToNote(noteId: Long, navOptions: NavOptions? = null) =
-    navigate(route = NoteRoute(noteId = noteId), navOptions = navOptions)
 
 fun NavGraphBuilder.noteScreen(
-    onBackClick: () -> Unit,
     onShowSnackbar: suspend (String, String?) -> Boolean
 ) {
-    composable<NoteRoute> {
-        NoteRoute(onBackClick = onBackClick)
+    composable<Route.Note> {
+        NoteRoute()
     }
 }
