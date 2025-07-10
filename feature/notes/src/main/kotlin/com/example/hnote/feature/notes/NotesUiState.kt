a2092c315eb87977213802a3d85c456b7c7c83e3
hmzgt <hmzgtl16@gmail.com>
2025-07-10 18:14:03 +0100

    Update NotesUiState
    The NotesUiState sealed interface is renamed to NotesState.
    A new data class NotesUiState is introduced to encapsulate NotesState along with other UI-related state properties like deletedNotes, selectedNotes, and isMultiSelectionEnabled.

--- feature/notes/src/main/kotlin/com/example/hnote/feature/notes/NotesUiState.kt
+++ feature/notes/src/main/kotlin/com/example/hnote/feature/notes/NotesUiState.kt
@@ -2,8 +2,14 @@ package com.example.hnote.feature.notes
 
 import com.example.hnote.core.model.Note
 
-sealed interface NotesUiState {
-
-    data object Loading : NotesUiState
-    data class Success(val notes: Map<Boolean, List<Note>>) : NotesUiState
+sealed interface NotesState {
+    data object Loading : NotesState
+    data class Success(val notes: Map<Boolean, List<Note>>) : NotesState
 }
+
+data class NotesUiState(
+    val notesState: NotesState = NotesState.Loading,
+    val deletedNotes: List<Note> = emptyList(),
+    val selectedNotes: List<Note> = emptyList(),
+    val isMultiSelectionEnabled: Boolean = false
+)
