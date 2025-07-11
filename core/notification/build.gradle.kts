f82887c9d6fb7f64742dfda8354fc160c83cc797
hmzgt <hmzgtl16@gmail.com>
2025-07-11 15:15:39 +0100

    add Notification module

---
+++ core/notification/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    alias(libs.plugins.hnote.android.library)
+    alias(libs.plugins.hnote.hilt)
+}
+
+android {
+    namespace = "com.example.hnote.core.notification"
+}
+
+dependencies {
+
+}
