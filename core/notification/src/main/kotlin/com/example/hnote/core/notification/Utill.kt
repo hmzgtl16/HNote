48d4abff4ac268b3907d9fe301bf3885c131536a
hmzgt <hmzgtl16@gmail.com>
2025-07-11 16:24:47 +0100

    Add notification resources and utility functions
    This commit introduces the necessary resources and utility functions for displaying notifications.
    
    It includes:
    - String resources for notification channel name, description, title, and content.
    - A vector drawable for the notification icon (`ic_notification.xml`) and its corresponding PNG versions for different screen densities.
    - Kotlin utility functions (`Utill.kt`) to:
        - Create a deep link URI.
        - Create a `Notification` object, ensuring the notification channel exists.
        - Create a `PendingIntent` for launching the target activity with a deep link.

---
+++ core/notification/src/main/kotlin/com/example/hnote/core/notification/Utill.kt
@@ -0,0 +1,61 @@
+package com.example.hnote.core.notification
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.net.toUri
+
+private const val DEEP_LINK_SCHEME_AND_HOST = "http://www.example.com/hnote"
+private const val NOTIFICATION_CHANNEL_ID = "hnote_notification_channel"
+private const val NOTIFICATION_REQUEST_CODE = 0
+private const val TARGET_ACTIVITY_NAME = "com.example.hnote.MainActivity"
+
+fun deepLink(id: Long): Uri = "$DEEP_LINK_SCHEME_AND_HOST/$id".toUri()
+
+fun Context.createNotification(deepLink: Uri): Notification {
+    ensureNotificationChannelExists()
+    return NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+        .apply {
+            setSmallIcon(R.drawable.ic_notification)
+            setContentTitle(getString(R.string.core_notification_notification_title))
+            setContentText(getString(R.string.core_notification_notification_description))
+            setContentIntent(taskPendingIntent(deepLink = deepLink))
+            setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            setAutoCancel(true)
+        }
+        .build()
+}
+
+private fun Context.ensureNotificationChannelExists() {
+
+    val channel = NotificationChannel(
+        NOTIFICATION_CHANNEL_ID,
+        getString(R.string.core_notification_channel_name),
+        NotificationManager.IMPORTANCE_DEFAULT,
+    ).apply {
+        description = getString(R.string.core_notification_channel_description)
+    }
+
+    NotificationManagerCompat
+        .from(this)
+        .createNotificationChannel(channel)
+}
+
+private fun Context.taskPendingIntent(deepLink: Uri): PendingIntent? =
+    PendingIntent.getActivity(
+        this,
+        NOTIFICATION_REQUEST_CODE,
+        Intent().apply {
+            action = Intent.ACTION_VIEW
+            data = deepLink
+            component = ComponentName(packageName, TARGET_ACTIVITY_NAME)
+        },
+        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+    )
