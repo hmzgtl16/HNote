c7df7396697c5643231f92b7d087ea40374dca30
hmzgt <hmzgtl16@gmail.com>
2025-07-11 11:37:12 +0100

    define SearchScreenEvent sealed interface

--- feature/search/src/main/kotlin/com/example/hnote/feature/search/SearchScreenEvent.kt
+++ feature/search/src/main/kotlin/com/example/hnote/feature/search/SearchScreenEvent.kt
@@ -1,5 +1,13 @@
 package com.example.hnote.feature.search
 
-sealed interface SearchScreenEvent {
+import com.example.hnote.core.model.SearchQuery
 
+sealed interface SearchScreenEvent {
+    data class SearchQueryChanged(val query: String) : SearchScreenEvent
+    data class SearchTriggered(val query: String) : SearchScreenEvent
+    data class ClearRecentSearch(val searchQuery: SearchQuery) : SearchScreenEvent
+    data object ClearAllRecentSearches : SearchScreenEvent
+
+    data class NavigateToNote(val noteId: Long) : SearchScreenEvent
+    data object NavigateBack : SearchScreenEvent
 }
