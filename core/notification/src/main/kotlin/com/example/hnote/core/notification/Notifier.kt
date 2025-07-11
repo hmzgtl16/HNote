d84a82655480f57906a3b710eb7bf089f6a2c3eb
hmzgt <hmzgtl16@gmail.com>
2025-07-11 17:54:14 +0100

    Refactor(core-notification): remove model dependency
    The `core-notification` module no longer depends on the `core-model` module.
    The `postReminderNotification` function in the `Notifier` interface and its implementation `NotifierImpl` now accept a `Long` id instead of a `Reminder` object.

--- core/notification/src/main/kotlin/com/example/hnote/core/notification/Notifier.kt
+++ core/notification/src/main/kotlin/com/example/hnote/core/notification/Notifier.kt
@@ -1,7 +1,5 @@
 package com.example.hnote.core.notification
 
-import com.example.hnote.core.model.Reminder
-
 interface Notifier {
-    fun postReminderNotification(reminder: Reminder)
+    fun postReminderNotification(id: Long)
 }
--- core/notification/src/main/kotlin/com/example/hnote/core/notification/NotifierImpl.kt
+++ core/notification/src/main/kotlin/com/example/hnote/core/notification/NotifierImpl.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.content.pm.PackageManager
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationManagerCompat
-import com.example.hnote.core.model.Reminder
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -14,17 +13,17 @@ import javax.inject.Singleton
 class NotifierImpl @Inject constructor(
     @ApplicationContext private val context: Context
 ) : Notifier {
-    override fun postReminderNotification(reminder: Reminder) {
+    override fun postReminderNotification(id: Long) {
         if (ActivityCompat.checkSelfPermission(
                 context,
                 Manifest.permission.POST_NOTIFICATIONS
             ) != PackageManager.PERMISSION_GRANTED
         ) return
 
-        val deepLink = deepLink(id = reminder.id)
+        val deepLink = deepLink(id = id)
         val notification = context.createNotification(deepLink = deepLink)
         NotificationManagerCompat
             .from(context)
-            .notify(reminder.id.toInt(), notification)
+            .notify(id.toInt(), notification)
     }
 }
