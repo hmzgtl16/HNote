b89bb075036ffd2c03e24faa46186191d0ed78dc
hmzgt <hmzgtl16@gmail.com>
2025-07-11 16:25:22 +0100

    Handle `POST_NOTIFICATIONS` permission
    This commit adds a check for the `POST_NOTIFICATIONS` permission before attempting to display a reminder notification. If the permission is not granted, the notification will not be shown.
    
    This change is necessary to comply with Android 13's new runtime permission for notifications.
    
    Additionally, this commit:
    - Creates a deep link for the notification.
    - Creates the notification using the context and deep link.
    - Uses `NotificationManagerCompat` to display the notification.

--- core/notification/src/main/kotlin/com/example/hnote/core/notification/NotifierImpl.kt
+++ core/notification/src/main/kotlin/com/example/hnote/core/notification/NotifierImpl.kt
@@ -1,6 +1,10 @@
 package com.example.hnote.core.notification
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationManagerCompat
 import com.example.hnote.core.model.Reminder
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
@@ -11,6 +15,16 @@ class NotifierImpl @Inject constructor(
     @ApplicationContext private val context: Context
 ) : Notifier {
     override fun postReminderNotification(reminder: Reminder) {
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) return
 
+        val deepLink = deepLink(id = reminder.id)
+        val notification = context.createNotification(deepLink = deepLink)
+        NotificationManagerCompat
+            .from(context)
+            .notify(reminder.id.toInt(), notification)
     }
 }
