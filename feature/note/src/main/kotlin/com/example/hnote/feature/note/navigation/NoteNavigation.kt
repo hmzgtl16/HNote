e4db1b669492bc54cd40021b2cd4636d5ac61995
hmzgt <hmzgtl16@gmail.com>
2025-07-13 13:38:17 +0100

    Implement deep link to note screen

--- feature/note/src/main/kotlin/com/example/hnote/feature/note/navigation/NoteNavigation.kt
+++ feature/note/src/main/kotlin/com/example/hnote/feature/note/navigation/NoteNavigation.kt
@@ -2,11 +2,15 @@ package com.example.hnote.feature.note.navigation
 
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import androidx.navigation.navDeepLink
+import com.example.hnote.core.navigation.DEEP_LINK_SCHEME_AND_HOST
 import com.example.hnote.core.navigation.Route
 import com.example.hnote.feature.note.NoteRoute
 
 fun NavGraphBuilder.noteScreen() {
-    composable<Route.Note> {
+    composable<Route.Note>(
+        deepLinks = listOf(navDeepLink<Route.Note>(basePath = DEEP_LINK_SCHEME_AND_HOST))
+    ) {
         NoteRoute()
     }
 }
