da918336243370b92e4220e1a5fdcb4a268fb4fa
hmzgt <hmzgtl16@gmail.com>
2025-07-14 17:21:52 +0100

    Switch core:common to JVM library
    The core:common module does not require Android-specific dependencies and can be a pure JVM library. This change updates the build configuration to reflect this.

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -1,10 +1,6 @@
 plugins {
-    alias(libs.plugins.hnote.android.library)
+    alias(libs.plugins.hnote.jvm.library)
     alias(libs.plugins.hnote.hilt)
-}
-
-android {
-    namespace = "com.example.hnote.core.navigation"
 }
 
 dependencies {
