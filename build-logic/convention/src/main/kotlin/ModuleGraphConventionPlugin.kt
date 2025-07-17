fa5ed63bf07fda85b55c3bd773f8538bd9f34733
hmzgt <hmzgtl16@gmail.com>
2025-07-17 16:08:51 +0100

    Remove ModuleGraph configuration
    The ModuleGraph configuration has been removed.
    The module name is now extracted from the displayName property.
    The theme has been changed to Theme.DARK.
    The graph heading has been updated to include the module name.
    The setupConfig has been simplified by removing the orientation, themeVariables, moduleTypes, and focusColor properties.

--- build-logic/convention/src/main/kotlin/ModuleGraphConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/ModuleGraphConventionPlugin.kt
@@ -1,6 +1,4 @@
 import com.example.hnotes.libs
-import dev.iurysouza.modulegraph.ModuleType
-import dev.iurysouza.modulegraph.Orientation
 import dev.iurysouza.modulegraph.Theme
 import dev.iurysouza.modulegraph.gradle.ModuleGraphExtension
 import org.gradle.api.Plugin
@@ -13,31 +11,19 @@ class ModuleGraphConventionPlugin : Plugin<Project> {
         with(target) {
             apply(plugin = libs.findPlugin("dev-iurysouza-modulegraph").get().get().pluginId)
 
+            val moduleName = displayName
+                .substring(startIndex = 8)
+                .replace(oldChar = Char(39), newChar = Char(96))
+
             extensions.configure<ModuleGraphExtension> {
                 graph(
                     readmePath = "${projectDir}/README.md",
-                    heading = "### Module Graph",
+                    heading = "# $moduleName Module",
                     setupConfig = {
                         showFullPath = true
                         setStyleByModuleType = true
-                        orientation = Orientation.LEFT_TO_RIGHT
                         nestingEnabled = true
-                        theme = Theme.BASE(
-                            themeVariables = mapOf(
-                                "primaryTextColor" to "#fff",
-                                "primaryColor" to "#5a4f7c",
-                                "primaryBorderColor" to "#5a4f7c",
-                                "lineColor" to "#f5a623",
-                                "tertiaryColor" to "#40375c",
-                                "fontSize" to "12px",
-                            ),
-                            moduleTypes = listOf(
-                                ModuleType.AndroidApp(),
-                                ModuleType.AndroidLibrary(),
-                                ModuleType.Kotlin()
-                            ),
-                            focusColor = "#FA8140"
-                        )
+                        theme = Theme.DARK
                     }
                 )
             }
