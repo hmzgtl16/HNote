b1965c914df29cdf4b24f5f7a17ff9ef97b4843a
hmzgt <hmzgtl16@gmail.com>
2025-07-19 12:34:51 +0100

    Add unit tests for NoteViewModel
    - Introduce `TestNoteRepository` and `TestNavigator` for testing purposes.
    - Add `NoteViewModelTest` to verify the initial state of `NoteViewModel`.

--- build-logic/convention/src/main/kotlin/HiltConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/HiltConventionPlugin.kt
@@ -10,21 +10,34 @@ class HiltConventionPlugin : Plugin<Project> {
             apply(plugin = libs.findPlugin("com-google-devtools-ksp").get().get().pluginId)
 
             dependencies {
-                add("ksp", libs.findLibrary("com-google-dagger-hilt-compiler").get())
+                add(
+                    "ksp",
+                    libs.findLibrary("com-google-dagger-hilt-compiler").get()
+                )
             }
 
             pluginManager.withPlugin(
                 libs.findPlugin("org-jetbrains-kotlin-jvm").get().get().pluginId
             ) {
                 dependencies {
-                    add("implementation", libs.findLibrary("com-google-dagger-hilt-core").get())
+                    add(
+                        "implementation",
+                        libs.findLibrary("com-google-dagger-hilt-core").get()
+                    )
                 }
             }
 
             pluginManager.withPlugin(libs.findPlugin("com-android-base").get().get().pluginId) {
                 apply(plugin = libs.findPlugin("com-google-dagger-hilt").get().get().pluginId)
                 dependencies {
-                    add("implementation", libs.findLibrary("com-google-dagger-hilt-android").get())
+                    add(
+                        "implementation",
+                        libs.findLibrary("com-google-dagger-hilt-android").get()
+                    )
+                    add(
+                        "testImplementation",
+                        libs.findLibrary("com-google-dagger-hilt-android-testing").get()
+                    )
                 }
             }
         }
